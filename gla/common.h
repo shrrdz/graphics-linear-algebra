d605c11bf7c10cbd2d0d70430346eacbf6367038
shredz <shredz@centrum.cz>
2024-01-19 20:26:51 +0100

    add various common math functions

---
+++ gla/common.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "gla.h"
+
+namespace gla
+{
+    static const float PI = 3.1415927F;
+
+    static float radians(const float degrees)
+    {
+        return degrees * (PI / 180);
+    }
+
+    static float degrees(const float radians)
+    {
+        return radians * (180 / PI);
+    }
+
+    template<typename T>
+    static T clamp(T value, const T min, const T max)
+    {
+        value = (value < min) ? min : (value > max) ? max : value;
+
+        return value;
+    }
+
+    template<typename T>
+    static T lerp(const T a, const T b, const float t)
+    {
+        return a + (b - a) * clamp(t, 0.0F, 1.0F);
+    }
+
+    template<typename T>
+    static float random(const T min, const T max)
+    {
+        return static_cast<T>(rand()) / (RAND_MAX / (max - min)) + min;
+    }
+}
--- gla/gla.h
+++ gla/gla.h
@@ -4,10 +4,13 @@
 
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
+#include <algorithm>
 #include <type_traits>
 
 #include "assert.h"
+#include "common.h"
 #include "forward.h"
 
 #include "vector.h"
